acebb26290b5550709015e11fbff44804eea371f
Alejandro Carmona <acarmona22@gmail.com>
2012-10-08 23:11:56 -0500

    Layout Fixed

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -15,7 +15,7 @@ class UserSessionsController < ApplicationController
 
     respond_to do |format|
       if @user_session.save
-        format.html { redirect_to users_url, notice: 'Successfully logged in.' }
+        format.html { redirect_to orders_url, notice: 'Successfully logged in.' }
         format.json { render json: @user_session, status: :created, location: @user_session }
       else
         format.html { render action: "new" }
