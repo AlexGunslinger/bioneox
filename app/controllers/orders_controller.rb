9043f5a187918ea3579e9a8a4c2139f626a76205
Alejandro Carmona <acarmona22@gmail.com>
2012-11-09 05:10:27 -0600

    Call action from get to post

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_filter :require_user
+  before_filter :require_user, :except => [:call]
   before_filter :set_nav_top
   layout "samples", :except => [:update_order_type]
    
